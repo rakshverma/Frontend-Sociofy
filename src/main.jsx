e39ac057b5341409ce033affd393272d5f040b8b
rakshverma <146798265+rakshverma@users.noreply.github.com>
2025-03-27 13:01:24 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    if (import.meta.env.MODE === 'production') {
+if (import.meta.env.MODE === 'production') {
   console.log = () => {};
   console.warn = () => {};
   console.error = () => {};
 }
 
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
