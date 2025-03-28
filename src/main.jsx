8e1642ca60d9ad9daae4c64794338e53a88a13f1
rakshverma <146798265+rakshverma@users.noreply.github.com>
2025-03-28 13:46:36 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,4 @@
-// Disable all console methods in production
-if (process.env.NODE_ENV === 'production') {
-  // Override console methods
-  Object.keys(console).forEach((method) => {
-    console[method] = function () {
-      // Do nothing
-    };
-  });
 
-  // Optional: Disable access to console altogether
-  Object.defineProperty(window, 'console', {
-    configurable: false,
-    get: function () {
-      throw new Error('Console access is disabled for security reasons.');
-    }
-  });
-}
-
-// Import React and ReactDOM after disabling console
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
